f5f3e7065ae64ecf6a2d62a86fab10f1faa49e1c
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-03-30 02:01:04 -0300

    chore: fixing server code

--- gchtoken/server/server.py
+++ gchtoken/server/server.py
@@ -2,8 +2,13 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/token', methods=['GET', 'POST']):
+@app.route('/token', methods=['GET', 'POST'])
+def redeem_token():
     if request.method == 'POST':
-        print('POST')
+        print(f"Token: {request.form['token']}")
+        print(f"Password: {request.form['password']}")
 
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
